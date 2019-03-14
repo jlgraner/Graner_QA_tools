fbf3ce6d33ff3a5356523654465e782e011e7e6d
jlgraner <32852039+jlgraner@users.noreply.github.com>
2019-03-14 17:17:07 -0400

    Update quickqa_nipype_wf.py

--- quickqa_nipype_wf.py
+++ quickqa_nipype_wf.py
@@ -1,36 +1,52 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import os
-import subprocess
+#Write the temporary output images to a nipype directory structure
+#Create jpegs and gifs
+#Delete the temporary output images
+
+import sys, os
 from nipype import Workflow, Function, Node
 from nipype.interfaces import afni
-
 from PIL import Image as pImage
 from IPython.display import Image as ipyImage
 
-this_env = os.environ
-my_dir = this_env['MYDIR']
+args = sys.argv
 
-#Set up test data set and base output directory
-input_func_data = os.path.abspath(os.path.join(my_dir, 'Data/Graner_QA_tools/test_data/fmri/fmri_test_input.nii.gz'))
-base_dir = os.path.join(my_dir, 'Data/Graner_QA_tools/test_output/QA_tools_dev_space/')
+def _check_inputs(args):
+    if len(args) < 2:
+        print('Requires one input argument: file name of input .nii or .nii.gz image')
+        return 0
+    return 1
+
+def _format_input_file(raw_input_file):
+    #Check to see if the file was passed without a path.
+    #If no path was included, append the current working directory.
+    if os.path.split(raw_input_file)[0] == '':
+        input_func_data = os.path.join(os.getcwd(), raw_input_file)
+    else:
+        input_func_data = raw_input_file
+    return input_func_data
 
 #Define the wrapper function to 3dTcat (that can accept an input file name that doesn't fully exist)
 def cutTRs(input_data):
     import subprocess
-    import os
-    full_input_file = input_data+'[4..$]'
-    input_name = os.path.split(input_data)[-1]
-    prefix = "_cut"
-    if input_name[-7:] == '.nii.gz':
-        cut_output = input_name.split('.nii.gz')[0]+prefix+input_name[-7:]
-    elif input_name[-4:] == '.nii':
-        cut_output = input_name.split('.nii')[0]+prefix+input_name[-4:]
-    else:
+    import os, string
+
+    #Make sure the input data file ends in .nii or .nii.gz
+    if not ((input_data[-7:] == '.nii.gz') or (input_data[-4:] == '.nii')):
         raise RuntimeError('Input data file does not end in .nii or .nii.gz! -- cutTRs()')
+
+    full_input_file = input_data+'[4..$]'
+    #Create the output file path/name
+    input_path, input_name = os.path.split(input_data)
+    suffix = "_cut"
+    file_part = input_name.split('.')[0]
+    # ending_part = string.join(input_name.split('.')[1:], '.')
+    ending_part = '.'.join(input_name.split('.')[1:])
+    new_name = file_part+suffix+'.'+ending_part
     
-    cut_output = os.path.join(os.getcwd(), cut_output)
+    cut_output = os.path.join(os.getcwd(), new_name)
     
     call = ['3dTcat',
             '-prefix', cut_output,
@@ -40,41 +56,58 @@ def cutTRs(input_data):
     out, err = process.communicate()
     return cut_output
 
+def main(args):
+    
+    #Check input arguments
+    args_okay = _check_inputs(args)
+    if not args_okay:
+        ##TODO: Add error message output
+        raise RuntimeError
 
-#Instantiate AFNI 3dTcat interface
-#tcatNode = afni.TCat()
-cutNode = Node(Function(input_names=['input_data'],
-                    output_names=['cut_output'],
-                    function=cutTRs),
-           name='cut')
+    #Extract the passed argument as the input file
+    raw_input_file = args[1]
 
-#Set the input value for the cut node
-cutNode.inputs.input_data = input_func_data
+    #If the input file name wasn't passed with a path, append the
+    #current working directory.
+    input_func_data = _format_input_file(raw_input_file)
 
-#Instantiate an AFNI 3dTstat node for calculating standard deviation
-stdevNode = Node(afni.TStat(args='-stdev', outputtype='NIFTI_GZ'), name='stdev')
+    #Create the nipype base directory based on the location of this file
 
-#Instantiate an AFNI 3dTstat node for calculating mean
-meanNode = Node(afni.TStat(args='-mean', outputtype='NIFTI_GZ'), name='mean')
 
-#Instantiate an AFNI 3dCalc node for calculating temporal SNR
-snrNode = Node(afni.Calc(expr='abs(a)/b', args='-float', outputtype='NIFTI_GZ'), name='snr')
+    #Set up test data set and base output directory
+    base_dir = os.path.join(my_dir, 'Data/Graner_QA_tools/test_output/QA_tools_dev_space/')
 
-#Create a short workflow putting together the cut and stdev
-wf = Workflow(name='quickstats', base_dir=base_dir)
-#wf.connect(cut, 'cut_output', stdev, 'in_file')
-wf.connect([
-    (cutNode, stdevNode, [('cut_output', 'in_file')]),
-    (cutNode, meanNode, [('cut_output', 'in_file')]),
-    (meanNode, snrNode, [('out_file', 'in_file_a')]),
-    (stdevNode, snrNode, [('out_file', 'in_file_b')])
-          ])
+    #Instantiate AFNI 3dTcat interface
+    #tcatNode = afni.TCat()
+    cutNode = Node(Function(input_names=['input_data'],
+                        output_names=['cut_output'],
+                        function=cutTRs),
+               name='cut')
 
-#Create a flat graph of the workflow
-wf.write_graph(graph2use='flat')
+    #Set the input value for the cut node
+    cutNode.inputs.input_data = input_func_data
 
-flat_graph_file = os.path.join(base_dir, 'quickstats', 'graph_detailed.png')
-ipyImage(filename=flat_graph_file)
+    #Instantiate an AFNI 3dTstat node for calculating standard deviation
+    stdevNode = Node(afni.TStat(args='-stdev', outputtype='NIFTI_GZ'), name='stdev')
 
-#Run the workflow
-wf.run()
+    #Instantiate an AFNI 3dTstat node for calculating mean
+    meanNode = Node(afni.TStat(args='-mean', outputtype='NIFTI_GZ'), name='mean')
+
+    #Instantiate an AFNI 3dCalc node for calculating temporal SNR
+    snrNode = Node(afni.Calc(expr='abs(a)/b', args='-float', outputtype='NIFTI_GZ'), name='snr')
+
+    #Create a short workflow putting together the cut and stdev
+    wf = Workflow(name='quickstats', base_dir=base_dir)
+    #wf.connect(cut, 'cut_output', stdev, 'in_file')
+    wf.connect([
+        (cutNode, stdevNode, [('cut_output', 'in_file')]),
+        (cutNode, meanNode, [('cut_output', 'in_file')]),
+        (meanNode, snrNode, [('out_file', 'in_file_a')]),
+        (stdevNode, snrNode, [('out_file', 'in_file_b')])
+              ])
+
+    #Run the workflow
+    wf.run()
+
+if __name__ is "__main__":
+    main(args)
