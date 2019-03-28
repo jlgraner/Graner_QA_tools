b6af7d0f68285d4a7cf6abb0e33f5e698d35d659
jlgraner <32852039+jlgraner@users.noreply.github.com>
2019-03-28 12:55:27 -0400

    Create mri_quickgifs.py

---
+++ mri_quickgifs.py
@@ -0,0 +1,16 @@
+
+
+#Goal: generate some quick gifs of an input image and a few QC derivatives
+#      to visually check basic image quality
+
+#Read in nifti image
+#Create temporal standard deviation image
+#Create temporal mean image
+#Create temporal SNR image
+#Create gif of center saggital slices
+#Create gif of center axial slices
+#Create gif of center coronal slices
+#Create gif going through mean image axially
+#Create gif going through mean image saggitally
+#Create gif going through mean image coronally
+
